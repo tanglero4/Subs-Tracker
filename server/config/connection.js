7a1d88c97c4c84b5ebd8b6fe617d759d7fbc41e0
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-03-09 14:28:27 -0500

    Starter file structure

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || '',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
