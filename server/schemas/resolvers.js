1d8ce004fc86f041416006ea1da797af52d275ef
Jartius <JStavey13@users.noreply.github.com>
2023-03-10 14:42:30 -0500

    added to resolver and typedef

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,18 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+// const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        
+    }
+
+
+
+
+
+}
+
+
+
+module.exports = resolvers
