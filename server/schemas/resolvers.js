148fd74019c380f3032ac597b15e41acd7d8de47
Jartius <JStavey13@users.noreply.github.com>
2023-03-10 23:05:21 -0500

    added to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,30 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const { User, Subscriptions } = require("../models");
 // const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
-        
+        allSubs: async () => {
+            return Subscriptions.find({});
+          },
+        findSub: async (parent, { _id }) => {
+        const params = _id ? { _id } : {};
+        return Subscriptions.find(params);
+        },
+    },
+    Mutation: {
+        newSub: async (parent, args) => {
+            const subscription = await Subscriptions.create(args);
+            return subscription;
+          },
+        deleteSub: async (parent, { _id }) => {
+        return Subscriptions.findOneAndDelete({ _id: _id });
+        },
+        updateSubscription: async (parent, { id, input }) => {
+            const subscription = await Subscriptions.findByIdAndUpdate(id, input, { new: true });
+            return subscription;
+          },
     }
-
 
 
 
