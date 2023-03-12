9b2e38c73f31207ded048ea0cb46c961befd7b0d
Lou .A <loualicea@hotmail.com>
2023-03-12 08:04:01 -0400

    created queries for login, subscriptions and subscription total cost

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,60 +1,31 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
+query user($userId: ID!) {
+  user(userId: $userId) {
+    _id
+    username
+    email
+    subCount
+    subs {
       _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-      }
+      subName
+      description
     }
   }
+}
 `;
 
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
+export const QUERY_SUB = gql`
+  query getSub {
+  subs {
+    _id
+    subName
+    subCost
     }
-  }
+}
 `;
 
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-      }
-    }
-  }
-`;
 
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
-    }
-  }
-`;
+
+
